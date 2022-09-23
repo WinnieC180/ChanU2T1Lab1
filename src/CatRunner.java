0f1e86d3f3c708baf4b2c75d00c4c20ac116fe09
student <raakinb@nycstudents.net>
2022-09-23 12:33:02 -0400

    finish at home

--- src/CatRunner.java
+++ src/CatRunner.java
@@ -1,2 +1,14 @@
-package PACKAGE_NAME;public class CatRunner {
+public class CatRunner {
+    public static void main(String[] arg){
+
+        System.out.println();
+        Cat cat1 = new Cat("Kitty", 4, 4.2);
+        cat1.introduce();
+        cat1.printCatInfo();
+
+        System.out.println();
+        Cat cat2 = new Cat("Chubby", 8, 9.2);
+        cat2.introduce();
+        cat2.printCatInfo();
+    }
 }
